c0d6b3cbca098b54235b73a33c6ce8849b81925e
tusshar17 <tr.jakhara019@gmail.com>
2024-08-12 14:15:26 +0530

    nginx added

--- django.sh
+++ django.sh
@@ -11,5 +11,9 @@ echo "Migrate"
 python manage.py migrate
 echo "================================================="
 
+echo "Collect Static Files"
+python manage.py collectstatic --noinput
+echo "================================================="
+
 echo "Start Server"
 gunicorn backend.wsgi:application --config gunicorn_config.py
